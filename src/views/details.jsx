30e600ec9fe2804742035861787a8531725f1d29
Steve Rogers <steve.rog02@gmail.com>
2020-12-13 16:20:42 +0100

    details gfx

--- src/views/details.jsx
+++ src/views/details.jsx
@@ -4,6 +4,11 @@ import { Form, Heading, Card, Content, Media, Container, Columns, Box, Modal, Se
 import { NavBar, } from '../components/navbar.jsx';
 import Gravatar from 'react-gravatar'
 import { useParams } from 'react-router-dom';
+import Txid from '../assets/txid.svg'
+import Domain from '../assets/domain-white.svg'
+import Check from '../assets/check-white.svg'
+import Block from '../assets/block.svg'
+
 const ScryptaCore = require('@scrypta/core')
 const scrypta = new ScryptaCore(true)
 scrypta.staticnodes = true
@@ -106,97 +111,175 @@ export function Details(props) {
 
         async function transferName() {
             if (password.length > 0 && !isTransfering) {
-              setTransfering(true)
-              let master = await scrypta.readxKey(password, props.user.walletstore)
-              if (master !== false) {
-                let key = await scrypta.deriveKeyFromSeed(master.seed, "m/0")
-                let masterkey = await scrypta.importPrivateKey(key.prv, '-', false)
-                let balance = await scrypta.get('/balance/' + key.pub)
-                let validate = await scrypta.get('/validate/' + to)
-                if (balance.balance >= 0.001) {
-                if(validate.data.isvalid === true){
-                    let written = await scrypta.write(masterkey.walletstore, '-', 'transfer:' + uuid + ':' + to, '', '', 'names://')
-                    if (written.txs[0].length === 64) {
-                        alert('Name registered!')
-                        setShow(false)
-                        setTo("")
-                        setTransfering(false)
-                        setTimeout(async function () {
-                        window.location.reload()
-                        }, 1500)
+                setTransfering(true)
+                let master = await scrypta.readxKey(password, props.user.walletstore)
+                if (master !== false) {
+                    let key = await scrypta.deriveKeyFromSeed(master.seed, "m/0")
+                    let masterkey = await scrypta.importPrivateKey(key.prv, '-', false)
+                    let balance = await scrypta.get('/balance/' + key.pub)
+                    let validate = await scrypta.get('/validate/' + to)
+                    if (balance.balance >= 0.001) {
+                        if (validate.data.isvalid === true) {
+                            let written = await scrypta.write(masterkey.walletstore, '-', 'transfer:' + uuid + ':' + to, '', '', 'names://')
+                            if (written.txs[0].length === 64) {
+                                alert('Name registered!')
+                                setShow(false)
+                                setTo("")
+                                setTransfering(false)
+                                setTimeout(async function () {
+                                    window.location.reload()
+                                }, 1500)
+                            } else {
+                                setTransfering(false)
+                                alert('Something goes wrong, please retry!')
+                            }
+                        } else {
+                            alert('Recipient address is not valid')
+                        }
                     } else {
                         setTransfering(false)
-                        alert('Something goes wrong, please retry!')
+                        alert('Not enough funds!')
                     }
-                }else{
-                    alert('Recipient address is not valid')
-                }
                 } else {
-                  setTransfering(false)
-                  alert('Not enough funds!')
+                    setTransfering(false)
+                    alert('Wrong password!')
                 }
-              } else {
-                setTransfering(false)
-                alert('Wrong password!')
-              }
             }
         }
 
         const returnTransferBox = () => {
             if (showTransfer) {
-              return <Modal show={showTransfer} onClose={() => setShow(false)}>
-                <Modal.Content style={{ textAlign: "center" }}>
-                  <Section style={{ backgroundColor: 'white' }}>
-                    <Heading>Transfer {blockchainData.domain}</Heading>
+                return <Modal show={showTransfer} onClose={() => setShow(false)}>
+                    <Modal.Content style={{ textAlign: "center" }}>
+                        <Section style={{ backgroundColor: 'white' }}>
+                            <Heading>Transfer {blockchainData.domain}</Heading>
                     Transfer this domain to a friend!<br /><br />
-                    <Input style={{ width: "100%!important", textAlign: "center" }} type="text" onChange={(evt) => { setTo(evt.target.value) }} placeholder="Insert recipient address" value={to} /><br></br>
-                    <Input style={{ width: "100%!important", textAlign: "center" }} type="password" onChange={(evt) => { setPassword(evt.target.value) }} placeholder="Insert wallet password" value={password} /><br></br><br></br>
-                    {!isTransfering ? <Button onClick={transferName} color="info">TRANSFER</Button> : <div>Transferring, please wait...</div>}
-                  </Section>
-                </Modal.Content>
-              </Modal >
+                            <Input style={{ width: "100%!important", textAlign: "center" }} type="text" onChange={(evt) => { setTo(evt.target.value) }} placeholder="Insert recipient address" value={to} /><br></br>
+                            <Input style={{ width: "100%!important", textAlign: "center", marginTop: "20px" }} type="password" onChange={(evt) => { setPassword(evt.target.value) }} placeholder="Insert wallet password" value={password} /><br></br><br></br>
+                            {!isTransfering ? <Button onClick={transferName} color="success">TRANSFER</Button> : <div>Transferring, please wait...</div>}
+                        </Section>
+                    </Modal.Content>
+                </Modal >
             }
-          }
+        }
+
+        const returnSellBox = () => {
+            if (isSelling) {
+                return <Modal show={isSelling} onClose={() => setSelling(false)}>
+                    <Modal.Content style={{ textAlign: "center" }}>
+                        <Section style={{ backgroundColor: 'white' }}>
+                            <Heading>Sell {blockchainData.domain}</Heading>
+                                this domain <br /><br />
+                            <Input style={{ width: "100%!important", textAlign: "center" }} type="text" onChange={(evt) => { setPrice(evt.target.value) }} placeholder="Insert Amount" value={price} /><br></br>
+                            <Input style={{ width: "100%!important", textAlign: "center", marginTop: "20px" }} type="password" onChange={(evt) => { setPassword(evt.target.value) }} placeholder="Insert wallet password" value={password} /><br></br><br></br>
+                            {!isSelling ? <Button onClick={placeSell} color="success">TRANSFER</Button> : <div>Transferring, please wait...</div>}
+                        </Section>
+                    </Modal.Content>
+                </Modal >
+            }
+        } // QUALCOSA NON VA :D.. Ma ci ho provato 
 
         return (
             <div className="Details">
                 {returnTransferBox()}
+                {returnSellBox()}
                 <NavBar />
-                <Container>
-                    <Columns style={{ marginTop: "100px" }}>
+                <Container style={{ marginTop: "120px" }}>
+                    <h1 style={{ fontSize: "30px" }}>Details of: <span style={{ fontWeight: 600 }}>{blockchainData.domain}</span></h1>
+                    <p style={{ fontSize: "16px" }}>Use the buttons below to transfer, donate or sell your domain. Simply enter the user's Scrypta Blockchain address, enter the amount and confirm!</p><br /><br />
+                    <Columns>
                         <Columns.Column>
-                            <h1 style={{ fontSize: "30px", fontWeight: "600" }}>Details of: {blockchainData.domain}</h1><br />
-                            <p style={{ fontSize: "16px" }}>Use the buttons next door to transfer, donate or sell your domain. Simply enter the user's Scrypta Blockchain address, enter the amount and confirm!</p><br /><br />
                             <Box>
                                 <Media>
                                     <Media.Item renderAs="figure" position="left">
-                                        <Gravatar style={{ borderRadius: "100px" }} email={uuid} />
+                                        <Gravatar style={{ borderRadius: "100px", marginTop: "10px" }} email={uuid} />
                                     </Media.Item>
                                     <Media.Item>
                                         <Content>
                                             <p style={{ marginTop: "5px" }}>
                                                 <small>UUID Domain Name</small><br />
-                                                <strong>{uuid}</strong>
+                                                <h1 style={{ marginTop: 0 }}>{uuid}</h1>
                                                 <br />
                                             </p>
                                         </Content>
                                     </Media.Item>
                                 </Media>
                             </Box>
-                            <Box>
-                                {blockchainData.domain}<br></br>
-                                {blockchainData.block}<br></br>
-                                {blockchainData.txid}<br></br>
-                                {blockchainData.date}
+                        </Columns.Column>
+                    </Columns>
+                    <Columns>
+                        <Columns.Column>
+                            <Box style={{ backgroundColor: "#0A5E7E" }}>
+                                <Media style={{ padding: "40px 0" }}>
+                                    <Media.Item>
+                                        <Image style={{ padding: "5px 15px" }} size={64} alt="64x64" src={Domain} />
+                                    </Media.Item>
+                                    <Media.Item >
+                                        <Content style={{ color: "white" }}>
+                                            <p>Domain Name</p>
+                                            <b>{blockchainData.domain}</b>
+                                        </Content>
+                                    </Media.Item>
+                                </Media>
                             </Box>
-                            <Button.Group>
-                                <Button color="info" onClick={() => { setShow(true) }}>Transfer</Button>
-                                <Button color="success">Sell</Button>
-                            </Button.Group>
+                        </Columns.Column>
+                        <Columns.Column>
+                            <Box style={{ backgroundColor: "#19A1BC" }}>
+                                <Media style={{ padding: "40px 0" }}>
+                                    <Media.Item>
+                                        <Image style={{ padding: "5px 15px" }} size={64} alt="64x64" src={Block} />
+                                    </Media.Item>
+                                    <Media.Item>
+                                        <Content style={{ color: "white" }}>
+                                            <p>Block</p>
+                                            <b>{blockchainData.block}</b>
+                                        </Content>
+                                    </Media.Item>
+                                </Media>
+                            </Box>
+                        </Columns.Column>
+                        <Columns.Column>
+                            <Box style={{ backgroundColor: "#429A98" }}>
+                                <Media style={{ padding: "40px 0" }}>
+                                    <Media.Item>
+                                        <Image style={{ padding: "5px 15px" }} size={64} alt="64x64" src={Txid} />
+                                    </Media.Item>
+                                    <Media.Item>
+                                        <Content style={{ color: "white" }}>
+                                            <p>Blockchain Transaction</p>
+                                            <b>{blockchainData.txid}</b>
+                                        </Content>
+                                    </Media.Item>
+                                </Media>
+                            </Box>
+                        </Columns.Column>
+                        <Columns.Column>
+                            <Box style={{ backgroundColor: "#8EC6C5" }}>
+                                <Media style={{ padding: "40px 0" }}>
+                                    <Media.Item>
+                                        <Image style={{ padding: "5px 15px" }} size={64} alt="64x64" src={Check} />
+                                    </Media.Item>
+                                    <Media.Item>
+                                        <Content style={{ color: "white" }}>
+                                            <p>Timestamp</p>
+                                            <b>{blockchainData.date}</b>
+                                        </Content>
+                                    </Media.Item>
+                                </Media>
+                            </Box>
+                        </Columns.Column>
+                    </Columns>
+                    <Columns>
+                        <Columns.Column>
+                            <Button style={{width: "100%", height: "80px", fontSize: "22px"}} color="success" onClick={() => { setShow(true) }}>Transfer</Button>
+                        </Columns.Column>
+                        <Columns.Column>
+                            <Button style={{width: "100%", height: "80px", fontSize: "22px"}} color="danger" onClick={() => { setSelling(true) }}>Sell</Button>
                         </Columns.Column>
                     </Columns>
                 </Container>
             </div>
         );
+
     }
 }
